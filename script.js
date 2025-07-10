bdfaf2c937676ef799e03ceff4284d3d04bc4519
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-10 16:45:15 +0000

    Implement 'New Grid' button with grid clearing and size prompt
    In script.js
        - Selected resetButton.
        - Created clearGrid helper function.
        - Modified createGrid to call clearGrid at the
        begining.
        - Created handleNewGridClick function to
        manage the prompt, input validation(max 100,
        numbers only), and call createGrid with
        newSize.
        - Attached handleNewGridClick to resetButton's
        click event.

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
-const gridContainer = document.getElementById('gridContainer');
+const gridContainer = document.getElementById( 'gridContainer' );
+const resetButton = document.getElementById( 'resetButton' );
 
 
-const createGrid = ( sidesPerSquare ) => {
+const clearGrid = () => {
+    while ( gridContainer.firstChild ) {
+        gridContainer.removeChild( gridContainer.firstChild );
+    }
+}
+
+const handleNewGridClick = () => {
+    let newSize = null;
+    let isValidInput = false;
+
+    while ( !isValidInput ) {
+        const userInput = prompt('How many squares per side for the new grid');
+        
+        if(userInput === null){
+            return;
+        }
+
+        const num = parseInt( userInput );
+
+        if ( isNaN(num) || num <= 0 || num > 100 ) {
+            alert('Invalid input. Please enter a number between 1 and 100');
+        } else {
+            newSize = num;
+            isValidInput = true;
+        }
+    }
+    
+    if ( newSize !== null) {
+        createGrid(newSize);
+    }
+
+}
+
+resetButton.addEventListener('click', handleNewGridClick);
+
+const createGrid = (sidesPerSquare ) => { 
+    clearGrid();
+
     let times = sidesPerSquare * sidesPerSquare;
     const containerSize = 960;
     const squareSize = containerSize / sidesPerSquare;
 
-    for (let i = 0; i < times; i++) {
-        const square = document.createElement('div');
+    for ( let i = 0; i < times; i++ ) {
+        const square = document.createElement( 'div' );
         
-        square.classList.add('.grid-square');
-        square.style.width = `${squareSize}px`;
-        square.style.height = `${squareSize}px`;
+        square.classList.add( '.grid-square' );
+        square.style.width = `${ squareSize }px`;
+        square.style.height = `${ squareSize }px`;
 
-        square.addEventListener('mouseover', (event) => {
-            event.target.classList.add('colored');
+        square.addEventListener( 'mouseover', ( event ) => {
+            event.target.classList.add( 'colored' );
         })
 
-        gridContainer.appendChild(square);
+        gridContainer.appendChild( square );
     }
 
 
 };
 
-createGrid(16)
+createGrid( 16 );
