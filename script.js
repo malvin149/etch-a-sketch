d4b27e10304f1dcd1f35bb335c060252f3b43e75
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-10 11:28:23 +0000

    Add mouseover effect to change square color
    In script.js
        Added mouseover event listener to each square
        inside createGrid.
        Used event.target.classList.add('colored').
    In style.css
        Defined .colored class with black color.

--- script.js
+++ script.js
@@ -8,12 +8,19 @@ const createGrid = ( sidesPerSquare ) => {
 
     for (let i = 0; i < times; i++) {
         const square = document.createElement('div');
+        
         square.classList.add('.grid-square');
-
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
+
+        square.addEventListener('mouseover', (event) => {
+            event.target.classList.add('colored');
+        })
+
         gridContainer.appendChild(square);
     }
+
+
 };
 
 createGrid(16)
