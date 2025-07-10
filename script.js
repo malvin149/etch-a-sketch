08e4d5a1fd0362a1b2460a93440ce9e81e71625e
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-10 19:05:19 +0000

    Implement progressive darkening drawing effect
    Modified the mouseover event listener to track
    hits(dataset.hits).
    Calculated and applied event.target.style.opacity

--- script.js
+++ script.js
@@ -2,11 +2,60 @@ const gridContainer = document.getElementById( 'gridContainer' );
 const resetButton = document.getElementById( 'resetButton' );
 
 
+const MAX_GRID_SIZE = 100;
+const INITIAL_GRID_SIZE = 16;
+
 const clearGrid = () => {
     while ( gridContainer.firstChild ) {
         gridContainer.removeChild( gridContainer.firstChild );
     }
 }
+
+// Random Color function
+const getRandomRgbColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+};
+
+
+const createGrid = ( sidesPerSquare ) => { 
+    clearGrid();
+
+    let times = sidesPerSquare * sidesPerSquare;
+    const containerSize = 960;
+    const squareSize = containerSize / sidesPerSquare;
+
+    for ( let i = 0; i < times; i++ ) {
+        const square = document.createElement( 'div' );
+        square.classList.add( '.grid-square' );
+        
+        square.style.width = `${ squareSize }px`;
+        square.style.height = `${ squareSize }px`;
+        square.dataset.hits = 0;
+        
+        square.addEventListener( 'mouseover', ( event ) => {
+            // event.target.classList.add( 'colored' );
+            let hits = parseFloat(event.target.dataset.hits) || 0;
+            hits++;
+            
+            event.target.dataset.hits = hits;
+            let opacity = hits / 10;
+            if (opacity > 1) {
+                opacity = 1;
+            }
+            event.target.style.opacity = opacity;
+            // event.target.style.backgroundColor = getRandomRgbColor();
+            event.target.style.backgroundColor = 'black';
+        })
+
+        gridContainer.appendChild( square );
+    }
+
+};
+
+
 
 const handleNewGridClick = () => {
     let newSize = null;
@@ -21,8 +70,8 @@ const handleNewGridClick = () => {
 
         const num = parseInt( userInput );
 
-        if ( isNaN(num) || num <= 0 || num > 100 ) {
-            alert('Invalid input. Please enter a number between 1 and 100');
+        if ( isNaN(num) || num <= 0 || num > MAX_GRID_SIZE ) {
+            alert(`Invalid input. Please enter a number between 1 and ${MAX_GRID_SIZE}`);
         } else {
             newSize = num;
             isValidInput = true;
@@ -35,30 +84,9 @@ const handleNewGridClick = () => {
 
 }
 
+
+createGrid( INITIAL_GRID_SIZE );
+
 resetButton.addEventListener('click', handleNewGridClick);
 
-const createGrid = (sidesPerSquare ) => { 
-    clearGrid();
 
-    let times = sidesPerSquare * sidesPerSquare;
-    const containerSize = 960;
-    const squareSize = containerSize / sidesPerSquare;
-
-    for ( let i = 0; i < times; i++ ) {
-        const square = document.createElement( 'div' );
-        
-        square.classList.add( '.grid-square' );
-        square.style.width = `${ squareSize }px`;
-        square.style.height = `${ squareSize }px`;
-
-        square.addEventListener( 'mouseover', ( event ) => {
-            event.target.classList.add( 'colored' );
-        })
-
-        gridContainer.appendChild( square );
-    }
-
-
-};
-
-createGrid( 16 );
